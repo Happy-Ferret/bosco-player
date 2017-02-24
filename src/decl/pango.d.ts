da6ca2898678f96c0a98c93bd5233a043716a01e
darkoverlordofdata <darkoverlordofdata@gmail.com>
2017-02-24 09:42:59 -0800

    daily

--- src/decl/pango.d.ts
+++ src/decl/pango.d.ts
@@ -6,6 +6,83 @@
 
 declare module "Pango" {
     import * as GObject from "GObject"
+
+    export function attr_background_alpha_new(alpha: number):any
+    export function attr_background_new(red: number, green: number, blue: number):any
+    export function attr_fallback_new(enable_fallback: boolean):any
+    export function attr_family_new(family: string):any
+    export function attr_foreground_alpha_new(alpha: number):any
+    export function attr_foreground_new(red: number, green: number, blue: number):any
+    export function attr_gravity_hint_new(hint: GravityHint):any
+    export function attr_gravity_new(gravity: Gravity):any
+    export function attr_letter_spacing_new(letter_spacing: number):any
+    export function attr_rise_new(rise: number):any
+    export function attr_scale_new(scale_factor: number):any
+    export function attr_stretch_new(stretch: Stretch):any
+    export function attr_strikethrough_color_new(red: number, green: number, blue: number):any
+    export function attr_strikethrough_new(strikethrough: boolean):any
+    export function attr_style_new(style: Style):any
+    export function attr_type_get_name(type: AttrType):string
+    export function attr_type_register(name: string):AttrType
+    export function attr_underline_color_new(red: number, green: number, blue: number):any
+    export function attr_underline_new(underline: Underline):any
+    export function attr_variant_new(variant: Variant):any
+    export function attr_weight_new(weight: Weight):any
+    export function bidi_type_for_unichar(ch: number):BidiType
+    export function _break(text: string, length: number, any: any, attrs: any[], attrs_len: number):void
+    export function config_key_get(key: string):string
+    export function config_key_get_system(key: string):string
+    export function default_break(text: string, length: number, any: any, attrs: any, attrs_len: number):void
+    export function extents_to_pixels(inclusive: any, nearest: any):void
+    export function find_base_dir(text: string, length: number):Direction
+    export function find_map(any: any, engine_type_id: number, render_type_id: number):any
+    export function find_paragraph_boundary(text: string, length: number, paragraph_delimiter_index: number, next_paragraph_start: number):void
+    export function font_description_from_string(str: string):any
+    export function get_lib_subdirectory():string
+    export function get_log_attrs(text: string, length: number, level: number, any: any, log_attrs: any[], attrs_len: number):void
+    export function get_mirror_char(ch: number, mirrored_ch: number):boolean
+    export function get_sysconf_subdirectory():string
+    export function gravity_get_for_any(any: any):Gravity
+    export function gravity_get_for_script(script: Script, base_gravity: Gravity, hint: GravityHint):Gravity
+    export function gravity_get_for_script_and_width(script: Script, wide: boolean, base_gravity: Gravity, hint: GravityHint):Gravity
+    export function gravity_to_rotation(gravity: Gravity):number
+    export function is_zero_width(ch: number):boolean
+    export function itemize(context: Context, text: string, start_index: number, length: number, attrs: any, cached_iter: any):any
+    export function itemize_with_base_dir(context: Context, base_dir: Direction, text: string, start_index: number, length: number, attrs: any, cached_iter: any):any
+    export function any_from_string(any: string):any
+    export function any_get_default():any
+    export function log2vis_get_embedding_levels(text: string, length: number, pbase_dir: Direction):number
+    export function lookup_aliases(fontname: string, families: string[], n_families: number):void
+    export function markup_parser_finish(context: any, attr_list: any, text: string, accel_char: number):boolean
+    export function markup_parser_new(accel_marker: number):any
+    export function module_register(module: any):void
+    export function parse_enum(type: number, str: string, value: number, warn: boolean, possible_values: string):boolean
+    export function parse_markup(markup_text: string, length: number, accel_marker: number, attr_list: any, text: string, accel_char: number):boolean
+    export function parse_stretch(str: string, stretch: Stretch, warn: boolean):boolean
+    export function parse_style(str: string, style: Style, warn: boolean):boolean
+    export function parse_variant(str: string, variant: Variant, warn: boolean):boolean
+    export function parse_weight(str: string, weight: Weight, warn: boolean):boolean
+    export function quantize_line_geometry(thickness: number, position: number):void
+    export function read_line(stream: any, str: any):number
+    export function reorder_items(logical_items: any):any
+    export function scan_int(pos: string, out: number):boolean
+    export function scan_string(pos: string, out: any):boolean
+    export function scan_word(pos: string, out: any):boolean
+    export function script_for_unichar(ch: number):Script
+    export function script_get_sample_any(script: Script):any
+    export function shape(text: string, length: number, any: any, glyphs: any):void
+    export function shape_full(item_text: string, item_length: number, paragraph_text: string, paragraph_length: number, any: any, glyphs: any):void
+    export function skip_space(pos: string):boolean
+    export function split_file_list(str: string):string[]
+    export function trim_string(str: string):string
+    export function unichar_direction(ch: number):Direction
+    export function units_from_double(d: number):number
+    export function units_to_double(i: number):number
+    export function version():number
+    export function version_check(required_major: number, required_minor: number, required_micro: number):string
+    export function version_string():string
+
+
     export class Context extends GObject.Object {
         changed():void
         get_base_dir():Direction
@@ -14,20 +91,20 @@ declare module "Pango" {
         get_font_map():FontMap
         get_gravity():Gravity
         get_gravity_hint():GravityHint
-        get_language():any
-        get_matrix():any
-        get_metrics(desc: any, language: any):any
+        get_any():any
+        get_any():any
+        get_metrics(desc: any, any: any):any
         get_serial():number
         list_families(families: FontFamily[], n_families: number):void
         load_font(desc: any):Font
-        load_fontset(desc: any, language: any):Fontset
+        load_fontset(desc: any, any: any):Fontset
         set_base_dir(direction: Direction):void
         set_base_gravity(gravity: Gravity):void
         set_font_description(desc: any):void
         set_font_map(font_map: FontMap):void
         set_gravity_hint(hint: GravityHint):void
-        set_language(language: any):void
-        set_matrix(matrix: any):void
+        set_any(any: any):void
+        set_any(any: any):void
     }
     export class Engine extends GObject.Object {
     }
@@ -38,11 +115,11 @@ declare module "Pango" {
     export class Font extends GObject.Object {
         describe():any
         describe_with_absolute_size():any
-        find_shaper(language: any, ch: number):EngineShape
-        get_coverage(language: any):any
+        find_shaper(any: any, ch: number):EngineShape
+        get_coverage(any: any):any
         get_font_map():FontMap
         get_glyph_extents(glyph: any, ink_rect: any, logical_rect: any):void
-        get_metrics(language: any):any
+        get_metrics(any: any):any
     }
     export class FontFace extends GObject.Object {
         describe():any
@@ -62,7 +139,7 @@ declare module "Pango" {
         get_shape_engine_type():string
         list_families(families: FontFamily[], n_families: number):void
         load_font(context: Context, desc: any):Font
-        load_fontset(context: Context, desc: any, language: any):Fontset
+        load_fontset(context: Context, desc: any, any: any):Fontset
     }
     export class Fontset extends GObject.Object {
         foreach(func: any, data: any):void
@@ -70,7 +147,7 @@ declare module "Pango" {
         get_metrics():any
     }
     export class FontsetSimple extends Fontset {
-        constructor(language: any)
+        constructor(any: any)
         append(font: Font):void
         size():number
     }
@@ -79,7 +156,7 @@ declare module "Pango" {
         context_changed():void
         copy():Layout
         get_alignment():Alignment
-        get_attributes():any
+        get_anys():any
         get_auto_dir():boolean
         get_baseline():number
         get_character_count():number
@@ -116,7 +193,7 @@ declare module "Pango" {
         is_wrapped():boolean
         move_cursor_visually(strong: boolean, old_index: number, old_trailing: number, direction: number, new_index: number, new_trailing: number):void
         set_alignment(alignment: Alignment):void
-        set_attributes(attrs: any):void
+        set_anys(attrs: any):void
         set_auto_dir(auto_dir: boolean):void
         set_ellipsize(ellipsize: EllipsizeMode):void
         set_font_description(desc: any):void
@@ -142,17 +219,17 @@ declare module "Pango" {
         draw_glyphs(font: Font, glyphs: any, x: number, y: number):void
         draw_layout(layout: Layout, x: number, y: number):void
         draw_layout_line(line: any, x: number, y: number):void
-        draw_rectangle(part: RenderPart, x: number, y: number, width: number, height: number):void
+        draw_any(part: RenderPart, x: number, y: number, width: number, height: number):void
         draw_trapezoid(part: RenderPart, y1_: number, x11: number, x21: number, y2: number, x12: number, x22: number):void
         get_alpha(part: RenderPart):number
         get_color(part: RenderPart):any
         get_layout():Layout
         get_layout_line():any
-        get_matrix():any
+        get_any():any
         part_changed(part: RenderPart):void
         set_alpha(part: RenderPart, alpha: number):void
         set_color(part: RenderPart, color: any):void
-        set_matrix(matrix: any):void
+        set_any(any: any):void
     }    
     export enum Alignment{
         LEFT,
@@ -161,7 +238,7 @@ declare module "Pango" {
     }
     export enum AttrType{
         INVALID,
-        LANGUAGE,
+        any,
         FAMILY,
         STYLE,
         WEIGHT,
